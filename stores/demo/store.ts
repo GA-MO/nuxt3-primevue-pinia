c4cb5e25199221438f9f5dc828576e076e400365
thawatchai <thawatchai.deartk@gmail.com>
2022-05-02 23:08:52 +0700

    Update

--- stores/demo/store.ts
+++ stores/demo/store.ts
@@ -1,15 +1,11 @@
 import { defineStore } from 'pinia'
 import actions from './actions'
+import { DemoState } from './models'
 
-// useStore could be anything like useUser, useCart
-// the first argument is a unique id of the store across your application
-export const useAppData = defineStore('appData', {
-  state: () => {
+export const useDemoState = defineStore('demo', {
+  state: (): DemoState => {
     return {
-      // all these properties will have their type inferred automatically
-      counter: 0,
-      name: 'Eduardo',
-      isAdmin: true,
+      name: '',
     }
   },
   actions,
